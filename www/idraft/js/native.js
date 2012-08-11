254b46b72f37bb588afbc29ae77ce1500addaca7
James McPherson <jemcphe@fullsail.edu>
2012-08-10 22:54:12 -0500

    Updating application… Have added native features, however jquery will not work in iOS and I have added to my whitelist.. not sure

--- www/idraft/js/native.js
+++ www/idraft/js/native.js
@@ -3,28 +3,40 @@ $(document).ready(function(){
 // Example JSONP request with jQuery
                   function loadESPN(){
                   $.ajax({
-                         url: "http://api.espn.com/v1/sports/football/news",
+                         url: "http://api.espn.com/v1/sports/football/nfl/news/headlines?limit=5&apikey=ahq2xtaj5bcm7en3ea4ugp85",
                          data: {
                          // enter your developer api key here
                          apikey: "{ahq2xtaj5bcm7en3ea4ugp85}",
                          // the type of data you're expecting back from the api
-                         _accept: "application/json"
+                         _accept: "application/json",
+                         limit: 5
                          },
+                         type: 'GET',
                          dataType: "jsonp",
                          success: function(data) {
-                         // create an unordered list of headlines
-                         var ul = $('<ul/>');
+                         console.log("JSON Data Loaded");
+                         var ul = $('#espnHeadlines');
+                         console.log(data);
                          $.each(data.headlines, function() {
                                 var li = $('<li/>').text(this.headline);
+                                var pTag = $('<p/>').text(this.description);
                                 ul.append(li);
+                                li.append(pTag);
+                                console.log("So Far So Good!");
                                 });
-                         // append this list to the document body
-                         $('body').append(ul);
+//                         for(var i=0, j=data.headlines; i<j; i++) {
+//                         console.log("almost");
+//                         var headline = data.headlines;
+//                         var makeSubList = $('<li></li>');
+//                         var makeSubLi = $( '<p>' + headline[0] + '</p>');
+//                         makeSubList.append(makeSubLi).appendTo('#espnHeadlines');
+//                         };
                          },
                          error: function() {
-                         // handle the error
+                         console.log("Didn't Work");
                          }
                          });
                   };
+                  
                   loadESPN();
 });
