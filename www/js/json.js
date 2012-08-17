91ae42b3c39b45502bd1ebca2fba6e420c2e0fae
James McPherson <jemcphe@fullsail.edu>
2012-08-16 22:42:52 -0500

    Week 3 Commit…. Added a few html pages and functionality for camera usage in iPad

---
+++ www/js/json.js
@@ -0,0 +1,63 @@
+/*JSON File
+ * VFW - Week 4
+ * James McPherson
+ * 1203
+ * 03/18/2012
+ */
+
+var json = {
+		"player1": {
+				"position": ["Position: ", "QB"],
+				"pname"	: ["Player Name: ", "Tony Romo"],
+				"team"		: ["Team Name: ", "Dallas Cowboys"],
+				"bye"			: ["Bye Week: ", "12/12/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "8"],
+				"notes"		: ["Notes: ", "Scrambler"]
+		},
+		"player2": {
+				"position": ["Position: ", "RB"],
+				"pname"	: ["Player Name: ", "Adrian Peterson"],
+				"team"		: ["Team Name: ", "Minnesota Vikings"],
+				"bye"			: ["Bye Week: ", "11/12/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "10"],
+				"notes"		: ["Notes: ", "Fast"]
+		},
+		"player3": {
+				"position": ["Position: ", "WR"],
+				"pname"	: ["Player Name: ", "Larry Fitzgerald"],
+				"team"		: ["Team Name: ", "Arizona Cardinals"],
+				"bye"			: ["Bye Week: ", "10/10/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "9"],
+				"notes"		: ["Notes: ", "Catches Everything"]
+		},
+		"player4": {
+				"position": ["Position: ", "TE"],
+				"pname"	: ["Player Name: ", "Rob Gronkowski"],
+				"team"		: ["Team Name: ", "New England Patriots"],
+				"bye"			: ["Bye Week: ", "11/12/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "9"],
+				"notes"		: ["Notes: ", "Bull"]
+		},
+		"player5": {
+				"position": ["Position: ", "K"],
+				"pname"	: ["Player Name: ", "Garret Hartley"],
+				"team"		: ["Team Name: ", "New Orleans Saints"],
+				"bye"			: ["Bye Week: ", "10/10/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "10"],
+				"notes"		: ["Notes: ", "Accurate Kicker"]
+		},
+		"player6": {
+				"position": ["Position: ", "DEF"],
+				"pname"	: ["Player Name: ", "SanFrancisco 49ers"],
+				"team"		: ["Team Name: ", "SanFrancisco 49ers"],
+				"bye"			: ["Bye Week: ", "12/12/2012"],
+				"starter"	: ["Starter: ", "Yes"],
+				"skill"			: ["Skill: ", "9"],
+				"notes"		: ["Notes: ", "Great Pass Rush, Shut Down Defense"]
+		}
+}
